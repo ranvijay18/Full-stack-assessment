5c82f17b239d120d4d5500e1cf4ac06765187473
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-28 12:46:29 +0530

    Update fetch-data.js

--- controllers/fetch-data.js
+++ controllers/fetch-data.js
@@ -2,15 +2,22 @@ const configData = require("../models/configuration")
 
 
 exports.getData =async (req, res) => {
+    try{
     const config = await configData.find({id: req.params.id})
-
     res.json(config);
-
+    }catch(error){
+        console.log(error);
+    }
 }
 
 exports.updateData = async (req,res) => {
+
+    try{
     const updateRemark = req.body.remark;
     const id = req.params.id;
     const config = await configData.findOneAndUpdate({id:id}, {remark: updateRemark});
     res.json(config.status);
+    }catch(error){
+        console.log(error);
+    }
 }
